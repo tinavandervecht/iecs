8598bac790027180a84a6f741bed4ce9d0bd6f99
N_Ireland <gfx@minigraphix.ca>
2016-11-03 08:43:04 -0400

    mostly finished navigation menu. added header and rows/cols for the main dash content.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 $(document).foundation();
 (function(){
+var current = document.querySelector('#mainNav .current');
+var mainNavLinks = document.querySelectorAll('#mainNav ul li a');
 
+
+function mainNavFunc(event){
+  event.preventDefault();
+  var it = event.currentTarget;
+  current.classList.remove('current');
+  it.classList.add('current');
+  current = it;
+}
+
+
+for(var i=0;i<mainNavLinks.length;i++){
+    mainNavLinks[i].addEventListener('click', mainNavFunc, false);
+}
 })();
