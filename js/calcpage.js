a4984f8342efaad6b25977aeb5c8d773a523be58
N_Ireland <gfx@minigraphix.ca>
2016-11-24 09:24:40 -0500

    added a disabled style that will prevent blocks that have a low safety rating (checked when pulled from server with php) from being selected and displaying additional information

--- js/calcpage.js
+++ js/calcpage.js
@@ -1,18 +1,22 @@
 (function(){
-var blocks = document.querySelectorAll(".block");
+var blocks = document.querySelectorAll(".block:not(.disabled)");
 var selected = null; //container for currently selected
 function openClose(event){
   event.preventDefault();
   var it = event.currentTarget;
   if(selected!=null && it!=selected){
-    TweenLite.to(selected, 1,{height:"6em"});
+    TweenLite.to(selected, 0.5,{height:"6em"});
+    if(selected!=null){
+      selected.classList.remove('selected');
+    }
   }
   if(it!=selected){
-    TweenLite.to(it, 1,{height:"18em"});
-    // it.classList.toggle('selected');
+    TweenLite.to(it, 0.5,{height:"18em"});
+    it.classList.add('selected');
     selected = it;
   }else{
-    TweenLite.to(it, 1,{height:"6em"});
+    TweenLite.to(it, 0.5,{height:"6em"});
+    selected.classList.remove('selected');
     selected = null;
   }
 }
