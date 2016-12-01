f6f44221be1fc7f7046f0050d0c5cb2a2f3dfc07
Adam Luxton <adze_202@hotmail.com>
2016-12-01 17:12:32 -0500

    Cleaning Up the System
    Removing some of the CI3 fluff that doesn't need to be in the structure.

--- application/views/history/view.php
+++
@@ -1,8 +0,0 @@
-<h2><?php echo "Hello ".$title."!"; ?></h2>
-
-        <h3><?php echo "Your Company name is " . $userInfo[0]['company_name'] . "."; ?></h3>
-        <div class="main">
-                <p><?php echo "Your contact email is " . $userInfo[0]['company_email']; ?></p>
-                <p><?php echo "Your last estimate took place at " . $userInfo[0]['estimate_date']; ?></p>
-                <p><?php echo "Your last expected flow was " . $userInfo[0]['estimate_expectedFlow']; ?></p>
-        </div>
