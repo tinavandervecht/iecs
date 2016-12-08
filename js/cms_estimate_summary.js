7ed606e38439232c8912f1612320b887841d84bd
N_Ireland <gfx@minigraphix.ca>
2016-12-08 12:18:09 -0500

    added a cms_estimates js file, edits to cms estimates page

---
+++ js/cms_estimate_summary.js
@@ -0,0 +1,41 @@
+(function(){
+var submittoIECS = document.querySelector("#saveit");
+var blocks = document.querySelectorAll(".block:not(.disabled)");
+var selected = null; //container for currently selected
+var selectedblock = document.querySelector("#selectedblock");
+function changeLeftBox(blockbox){
+  selectedblock.dataset.blockId = blockbox.querySelector('.blocktype p').innerHTML;
+  selectedblock.querySelector('.blockname').innerHTML = blockbox.querySelector('.blocktype').innerHTML;
+  selectedblock.querySelector('img').src = blockbox.querySelector('.blockdiagram').src;
+}
+function openClose(event){
+  event.preventDefault();
+  var it = event.currentTarget;
+  if(selected!=null && it!=selected){
+    TweenLite.to(selected, 0.5,{height:"6em"});
+    if(selected!=null){
+      selected.classList.remove('selected');
+    }
+  }
+  if(it!=selected){
+    TweenLite.to(it, 0.5,{height:"18em"});
+    it.classList.add('selected');
+    selected = it;
+  }else{
+    TweenLite.to(it, 0.5,{height:"6em"});
+    selected.classList.remove('selected');
+    selected = null;
+  }
+  changeLeftBox(it);
+}
+
+for(var i=0;i<blocks.length;i++){
+  if(blocks[i].classList.contains('highlight')){
+    TweenLite.to(blocks[i], 0.5,{height:"18em"});
+    selected = blocks[i];
+  }
+  blocks[i].addEventListener("click",openClose,false);
+}
+
+changeLeftBox(document.querySelector('.block.highlight'));
+})();
