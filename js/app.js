98c820d666f423b925fc04c9cfa25d8c702bfbdc
N_Ireland <gfx@minigraphix.ca>
2017-02-07 13:45:08 -0500

    changed function to actually delete the quote and the refresh the page

--- js/app.js
+++ js/app.js
@@ -21,10 +21,12 @@ try {
   var deletes = document.querySelectorAll(".rightButton a");
   for(var i=0;i<deletes.length;i++){
     deletes[i].addEventListener('click',function(){
+      var it = event.currentTarget;
       event.preventDefault();
       var r = confirm("Are you sure you want to delete?");
       if(r){
         //code for confirm (delete) here
+        window.location.href = it.href;
       }else{
         //code for cancel (not delete) here
       }
