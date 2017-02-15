2d241bc0c9cd76f533a57cc3c42dae57fa4115d2
N_Ireland <gfx@minigraphix.ca>
2017-02-15 12:36:16 -0500

    fixed some compatability issues and changed some classes in the views to prevent duplicate stylings.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,5 @@
 $(document).foundation();
 (function(){
-var current = document.querySelector('#mainNav .current');
-var mainNavLinks = document.querySelectorAll('#mainNav ul li a');
-
-
-function mainNavFunc(event){
-//  event.preventDefault();
-  var it = event.currentTarget;
-  current.classList.remove('current');
-  it.classList.add('current');
-  current = it;
-}
-
-
-for(var i=0;i<mainNavLinks.length;i++){
-    mainNavLinks[i].addEventListener('click', mainNavFunc, false);
-}
-
 try {
   var deletes = document.querySelectorAll(".rightButton a");
   for(var i=0;i<deletes.length;i++){
