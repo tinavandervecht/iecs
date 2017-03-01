7b0223edf664331dd9afa107bd7a9d0a53c6a4fa
N_Ireland <gfx@minigraphix.ca>
2017-03-01 14:54:06 -0500

    got the bar chart looking nice - charts/bar.html. need to redo line chart. can't get the data to switch, however.

--- statistics/js/bar.js
+++ statistics/js/bar.js
@@ -1,114 +1,131 @@
 (function(){
-  //BAR.JS
-  var graphEl = document.querySelector('.barGraph');
-  var INITIAL_WIDTH = graphEl.getAttribute('width');
-  var dataBar = [
-    {"label": "Jan","num" : 147},
-    {"label": "Feb","num" : 76},
-    {"label": "Mar","num" : 409},
-    {"label": "Apr","num" : 81},
-    {"label": "May","num" : 57},
-    {"label": "Jun","num" : 94},
-    {"label": "Jul","num" : 45}
-  ];
-    function maxVal(arr){
-  var m = 0;
-  for(var i=0;i<arr.length;i++){
-     if(arr[i].num>m){
-        m = arr[i].num;
-        console.log(m);
-      }
-    }
-    console.log(m + " Is max");
-    return m;
+  //LET'S TRY THIS AGAIN
+  // READY
+  // SET
+  console.log("Go!");
+var data = [
+  {"label":"MAR","value":34},
+  {"label":"APR","value":14},
+  {"label":"MAY","value":66},
+  {"label":"JUN","value":14},
+  {"label":"JUL","value":107},
+  {"label":"AUG","value":83},
+  {"label":"SEP","value":85},
+  {"label":"OCT","value":16},
+  {"label":"NOV","value":6},
+  {"label":"DEC","value":42},
+  {"label":"JAN","value":20},
+  {"label":"FEB","value":22},
+];
+
+var data2 = [
+  {"label":"MAR","value":14},
+  {"label":"APR","value":44},
+  {"label":"MAY","value":56},
+  {"label":"JUN","value":25},
+  {"label":"JUL","value":197},
+  {"label":"AUG","value":84},
+  {"label":"SEP","value":25},
+  {"label":"OCT","value":38},
+  {"label":"NOV","value":56},
+  {"label":"DEC","value":12},
+  {"label":"JAN","value":18},
+  {"label":"FEB","value":67},
+];
+// var svg = d3.select('.barGraph').selectAll('.bar').data(data);
+var dataset = 2;
+var barPad = 10;
+var height = 300;
+var width = 600;
+var barWidth = 20;
+var max = 0;
+var labels = [];
+for(var key in data){
+ var num = data[key].value;
+ labels.push(data[key].label);
+ if(num>max){
+   max = num;
+ }
+}
+var margins = {
+  top:10,
+  left:40,
+  right:10,
+  bottom:30
+}
+
+function load(){
+  var yScale = d3.scaleLinear()
+  .rangeRound([height-margins.bottom-margins.top,0])
+  .domain([0,max]);
+
+  var xScale = d3.scaleBand()
+  .range([barWidth+barPad,width-margins.left-margins.right-barWidth])
+  .domain(labels)
+  .paddingInner(1);
+
+  var yAxis = d3.axisLeft(yScale);
+  var xAxis = d3.axisBottom(xScale);
+  var svg = d3.select('.barGraph')
+  .append("g")
+    .attr("transform", "translate(" + margins.left*7/8 + ","+ margins.top+")")
+    .call(yAxis.ticks(4));
+    svg.append("g")
+        .attr("transform", "translate(" + 0 + ","+ (height-margins.bottom)+")")
+        .call(xAxis);
+  update();
+}
+function update(){
+  var dedata = data;
+  switch (dataset) {
+    case 1:
+          dedata = data;
+      break;
+      case 2:
+            dedata = data2;
+        break;
+    default:
+    dedata = data;
   }
-  var months = [];
-  for(var i=0; i< dataBar.length;i++){
-    months.push(dataBar[i].label);
+  var graph = d3.select('.barGraph');
+  var selection = graph.selectAll('.bar').data(dedata);
+  barWidth = (width-margins.right-margins.left)/data.length;
+  var selectionEnter = selection.enter().append('g').attr('class', 'bar');
+
+      selectionEnter.append('rect')
+        .attr('height', function(d){return (d.value/ max)*(height - margins.top - margins.bottom);})
+        .attr('x', function(d,i){return margins.left + i*barWidth+barPad/2;})
+        // .attr('y', function(d){return height-(d.value)-margins.bottom;})
+        .attr('y' , function(d){return (height-margins.bottom) - (d.value/ max)*(height - margins.top - margins.bottom);})
+        .attr('width', barWidth-barPad)
+        .attr('fill', function(d,i){return i%2 ? "#213D02" : "#589021"});
+
+        selectionEnter.append('text')
+        .attr('x', function(d,i){return margins.left + i*barWidth+barPad/2;})
+        .attr('y' , function(d){
+          var ratio = (d.value/ max)
+          if(ratio < 1 ){
+            return (height-margins.bottom) - ratio*(height - margins.top - margins.bottom);
+          }else{
+            return 0;
+          }
+        })
+        .text(function(d){return d.value;})
+        .attr('text-anchor', 'start')
+        .attr('dx', function(d){
+          return (barWidth-this.getBBox().width+barPad)/4;
+          })
+        .attr('dy', function(d){return d.value/max < 0.1 ? -10 : 22;})
+        .attr('fill', function(d){return d.value/max < 0.1 ? "#222" : "#eee";});
+    selection.exit().remove();
+graph.on('click', function(e, i){
+  if(dataset==1){
+    dataset = 2;
+  }else{
+    dataset = 1;
   }
-  var charts = d3.select('.barGraph');
-  var chartPad = {"top":20,"left":20,"right":20,"bottom":20};
-  var xScale = (parseInt(d3.select('.barGraph').style('width'))-chartPad.right) / dataBar.length;
-  console.log(months);
-  var xScale = d3.scale.Ordinal()
-          .domain([months])
-          .rangePoints([0,12]);
-  var h = parseInt(d3.select('.barGraph').style('height'));
-  var yScale = h / maxVal(dataBar);
-  var padding = xScale/6;
-        var bar = charts.selectAll('.bar')
-          .data(dataBar)
-          .enter().append('g')
-          .attr('class', '.bar');
-
-          bar.append('rect')
-          .attr('x',function(d,i){
-            return i * xScale + padding*2;
-          })
-          .attr('y', function(d){
-            return parseInt(d3.select('.barGraph').style('height')) - d.num*yScale + 0;
-          })
-          .attr('width',xScale-padding)
-          .attr('fill',function(d,i){return i%2 ? "#213D02" : "#589021";})
-          .attr('height', function(d){return d.num*yScale;});
-
-          bar.append('text')
-          .attr('x',function(d,i){
-            return i * xScale + padding*2;
-          })
-          .attr('y', function(d){
-            return parseInt(d3.select('.barGraph').style('height')) - d.num*yScale + 0;
-          })
-          .attr('text-anchor','middle')
-          .attr('dx', function(d){return padding*2.5;})
-          .attr('dy', function(d){
-            console.log(d.num*yScale/h);
-            if((d.num*yScale/h) < 0.2){
-              return -20;
-            }else{
-              return 40;
-            }
-          })
-          // .attr('dy', function(d,i){return h-20;})
-          // .attr('dx', function(d,i){return i * xScale + padding*4})
-          // .attr('dx', function(d,i){ return -i * xScale - (padding * 4);})
-          // .attr('transform','rotate(90)')
-          // .attr('fill',"#eee")
-          .attr('fill', function(d,i){
-            if((d.num*yScale/h) < 0.2){
-              return "#222";
-            }else{
-              return "#eee";
-            }
-            })
-          .attr('stroke-width', 0.8)
-          .text(function(d){ return d.num});
-
-          charts.append("g")
-            .attr("transform", "translate(0," + h + ")")
-            .call(d3.axisBottom(xScale));
-
-
-      // charts.enter()
-      //   .append('rect')
-      //   .attr('x',function(d,i){
-      //     return i * xScale + padding*2;
-      //   })
-      //   .attr('y', function(d){
-      //
-      //     return = parseInt(d3.select('.barGraph').style('height')) - d*yScale + 0;
-      //   })
-      //   .attr('width',xScale-padding)
-      //   .attr('fill',function(d,i){return i%2 ? "#213D02" : "#589021";})
-      //   .attr('height', function(d){return d*yScale;});
-        // window.addEventListener('resize',function(){
-        //   var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-        //   if(w>INITIAL_WIDTH){
-        //     scaleGraph(INITIAL_WIDTH);
-        //     // graphEl.setAttribute('width',INITIAL_WIDTH);
-        //   }else{
-        //     scaleGraph(w);
-        //     // graphEl.setAttribute('width',w);
-        //   }
-        // },false);
+    update();
+  });
+}
+load();
 })();
--- testing/js/tame.js
+++ testing/js/tame.js
@@ -41,8 +41,7 @@
                     var newData = data2;
 
                     var maxArr = d3.entries(data);
-                    // .sort(function(a, b) { return d3.descending(a.value.usage, b.value.usage); })[0];
-                    var max = 0;
+                        var max = 0;
                     for(var i=0;i<maxArr.length;i++){
                       if(maxArr[i].value.usage>max){
                       max = maxArr[i].value.usage;
@@ -77,22 +76,19 @@
                             bottom: 20,
                             left: 50
                         },
-                        // xScale = d3.scale.ordinal()
-                        // .domain(d3.range(10))
-                        // .rangeBands([50, 580]),
+                      
                         xScale = d3.scale.ordinal()
                         .range([visMargins.left, visWidth -  visMargins.right])
                         .domain(data.map(function(d){ return d.mon; }))
-                        // .domain([1,2,3,4])
                         .rangePoints([0.5,1.5,2.5,3.5])
                         .rangeBands([visMargins.left, visWidth -  visMargins.right]),
-                        // xScale = d3.scale.ordinal().range([1,2,3,4,5,6,7,8,9,10,11,12]).domain([1,2,3,4,5,6,7,8,9,10,11,12]),
-                        // xScale = d3.scale.ordinal().range([visMargins.left, visWidth - visMargins.right]).domain(months.),//.rangePoints([0,600]),
+                      
+                      
                         yScale = d3.scale.linear().range([visHeight - visMargins.top, visMargins.bottom]).domain([0, max]),
                         xAxis = d3.svg.axis()
                         .scale(xScale)
                         .orient("bottom"),
-                        // .tickFormat(d3.time.format("%M")),
+                        
                         yAxis = d3.svg.axis()
                         .scale(yScale)
                         .orient("left");
@@ -121,26 +117,7 @@
                                 return yScale(0);
                             });
 
-                        // .interpolate("basis");
-                        // vis.on("click",function(){
-                        //   vis.selectAll("path.line")
-                        //     .transition().duration(500)
-                        //     .attr('d',lineInit(currentData))
-                        //     .transition().duration(500).delay(500)
-                        //     .attr('d', lineGen(newData));
-                        //   // vis.append('svg:path')
-                        //   //     .attr('stroke', '#ABFF24')
-                        //   //     .attr('stroke-width', 2)
-                        //   //     .attr('fill', 'none')
-                        //   //     .attr('d', lineInit(data2))
-                        //   //     .transition().duration(500).delay(500)
-                        //   //     .attr('d', lineGen(data2));
-                        //   var temp = currentData;
-                        //   currentData = newData;
-                        //   newData = temp;
-                        // });
-                        // var path = vis.selectAll("path").data(lineGen(currentData));
-
+                      
                     vis.append('svg:path')
                         .attr('stroke', '#ABFF24')
                         .attr('class', 'line')
@@ -172,10 +149,10 @@
                             .attr("d", lineGen(currentData));
                           }
 
-                        // d3.select(window).on('resize', resize);
+                    
                 }
 
-                // window.addEventListener('load',initChart,false);
+                
 
                 initChart();
 
