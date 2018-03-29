566bdcae412858b92b058a93dda202ae77addc7f
Tina Vandervecht <tina.vandervecht@roomroster.com>
2018-03-29 16:25:45 -0400

    Ie consoles removed because why

--- js/ie.js
+++ js/ie.js
@@ -2,9 +2,7 @@
   var msie = document.documentMode;
 if (msie < 9) {
     // code for IE < 9
-    console.log("IE9");
     alert('Please consider <a href="browsehappy.com">updating</a> your browser for the best possible experience.</a>');
 }else{
-  console.log("not IE9");
 }
 })();
