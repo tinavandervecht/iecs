f8d3a620397543d9d69193d1c8de23e4d6e0e747
Tina Vandervecht <tina.vandervecht@roomroster.com>
2018-04-04 18:13:48 -0400

    Cleanup profile inputs

--- application/views/profile/view.php
+++ application/views/profile/view.php
@@ -11,19 +11,34 @@
         <div class="columns small-12 large-4">
           <div class="field">
             <h3 class="title">Contact Name</h3><?php echo form_error('contactName', '<span class="error">', '</span>');?>
-            <a href="#" class="value"><?php echo $userInfo['company_contactName'];?><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a>
+            <a href="#" class="value">
+                <span class="text">
+                    <?php echo $userInfo['company_contactName'];?>
+                </span>
+                <img src="<?php echo base_url('img/pencil_icon.svg');?>"/>
+            </a>
             <input class="value hidden" name="contactName" value="<?php echo $userInfo['company_contactName'];?>">
           </div>
           <div class="field">
             <h3 class="title">Company</h3><?php echo form_error('name', '<span class="error">', '</span>');?>
-            <a href="#" class="value"><?php echo $userInfo['company_name'];?><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a>
+            <a href="#" class="value">
+                <span class="text">
+                    <?php echo $userInfo['company_name'];?>
+                </span>
+                <img src="<?php echo base_url('img/pencil_icon.svg');?>"/>
+            </a>
             <input class="value hidden" name="name" value="<?php echo $userInfo['company_name'];?>">
           </div>
         </div>
         <div class="columns small-12 large-4 end">
           <div class="field">
             <h3 class="title">Phone Number</h3><?php echo form_error('phone', '<span class="error">', '</span>');?>
-            <a href="#" class="value"><?php echo $userInfo['company_phone'];?><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a>
+            <a href="#" class="value">
+                <span class="text">
+                    <?php echo $userInfo['company_phone'];?>
+                </span>
+                <img src="<?php echo base_url('img/pencil_icon.svg');?>"/>
+            </a>
             <input class="value hidden" name="phone" value="<?php echo $userInfo['company_phone'];?>">
           </div>
           <input id="saveProfile" class="greenButton hidden" name="submit" type="submit" value="Save Changes">
--- js/profile.js
+++ js/profile.js
@@ -1,18 +1,49 @@
 (function(){
+var inputs = document.querySelectorAll("input.value");
 var fields = document.querySelectorAll("a.value");
 var isForm = false;
 var lastClick;
-function toggleEdit(event){
-  event.preventDefault();
-  var it = event.currentTarget;
-  if(!isForm){
-    document.querySelector("#saveProfile").classList.remove('hidden');
-  }
-  it.classList.toggle('hidden');
-  it.parentNode.querySelector('input.value').classList.toggle('hidden');
-  it.parentNode.querySelector('input.value').focus();
+
+function enableEdit(event){
+    event.preventDefault();
+    var it = event.currentTarget;
+    if(!isForm){
+        document.querySelector("#saveProfile").classList.remove('hidden');
+    }
+    it.classList.toggle('hidden');
+    it.parentNode.querySelector('input.value').classList.toggle('hidden');
+    it.parentNode.querySelector('input.value').focus();
 }
+
+function removeEdit(event){
+    event.preventDefault();
+    var it = event.currentTarget;
+    it.parentNode.querySelector('input.value').classList.add('hidden');
+    it.parentNode.querySelector('a.value').classList.remove('hidden');
+}
+
+function updateLinkText(event){
+    event.preventDefault();
+    var it = event.currentTarget;
+    it.parentNode.querySelector('input.value').classList.add('hidden');
+    it.parentNode.querySelector('a.value').querySelector('.text').textContent = it.value;
+}
+
+/*
+ * foreach link on page, set event LISTENERS
+ * to hide link when clicked, and show next input
+ */
 for(var i=0;i<fields.length;i++){
-  fields[i].addEventListener('click', toggleEdit,false);
+    fields[i].addEventListener('click', enableEdit, false);
+}
+
+/*
+ * foreach input on page, set event LISTENERS
+ * to hide input when user focusses off input
+ * and to update link text as user updates input text
+ */
+for(var i=0;i<inputs.length;i++){
+    inputs[i].addEventListener('change', updateLinkText, false);
+    inputs[i].addEventListener('blur', removeEdit, false);
 }
 })();
