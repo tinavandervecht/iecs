d1f1f141fba6c7fa0cfad85c8a94de891cd67155
Tina Vandervecht <tina.vandervecht@roomroster.com>
2018-04-06 10:49:17 -0400

    moving step 4 into re-useable partial

--- application/views/quotes/new.php
+++ application/views/quotes/new.php
@@ -19,57 +19,10 @@
             <?php include 'application/views/quotes/partials/environment.php'; ?>
         </section>
 
-          <section class="pagnation-page" id="4">
-            <div class="row">
-              <div class="columns small-12">
-              <a href="#" id="goBack" data-pag="3" class="sectionTitle">&lt;&nbsp;Input Review</a>
-            </div>
-            <div class="columns small-12 medium-6">
-              <div class="summaryEntry" id="sum_details">
-                <h4 class="entryTitle">PROJECT DETAILS<a href="#" class="edit" data-pag="1"><span class="hide-for-mobile">Edit</span><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a></h4>
-                <p class="text"> Nothing here.</p>
-              </div>
-              <!-- end of summaryEntry -->
-              <div class="summaryEntry" id="sum_flow">
-                <h4 class="entryTitle">Flow and velocity<a href="#" class="edit" data-pag="1"><span class="hide-for-mobile">Edit</span><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a></h4>
-                <p class="text"> Nothing here.</p>
-              </div>
-              <!-- end of summaryEntry -->
-              <div class="summaryEntry" id="sum_slopes">
-                <h4 class="entryTitle">Slopes<a href="#" class="edit" data-pag="2"><span class="hide-for-mobile">Edit</span><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a></h4>
-                <p class="text"> Nothing here.</p>
-              </div>
-              <!-- end of summaryEntry -->
-              <div class="summaryEntry" id="sum_type">
-                <h4 class="entryTitle">Types of Flow<a href="#" class="edit" data-pag="2"><span class="hide-for-mobile">Edit</span><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a></h4>
-                <p class="text"> Nothing here.</p>
-              </div>
-              <!-- end of summaryEntry -->
-          </div>
-          <div class="columns small-12 medium-6">
-            <div class="summaryEntry" id="sum_bed">
-              <h4 class="entryTitle">Bed width and alignment<a href="#" class="edit" data-pag="2"><span class="hide-for-mobile">Edit</span><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a></h4>
-              <p class="text"> Nothing here.</p>
-            </div>
-            <!-- end of summaryEntry -->
-            <div class="summaryEntry" id="sum_channel">
-              <h4 class="entryTitle">Channel Specifications<a href="#" class="edit" data-pag="3"><span class="hide-for-mobile">Edit</span><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a></h4>
-              <p class="text"> Nothing here.</p>
-            </div>
-            <!-- end of summaryEntry -->
-            <div class="summaryEntry" id="sum_environment">
-              <h4 class="entryTitle">Environment<a href="#" class="edit" data-pag="3"><span class="hide-for-mobile">Edit</span><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a></h4>
-              <p class="text"> Nothing here.</p>
-            </div>
-            <!-- end of summaryEntry -->
-            <div class="summaryEntry" id="sum_comments">
-              <h4 class="entryTitle">Comments<a href="#" class="edit" data-pag="3"><span class="hide-for-mobile">Edit</span><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a></h4>
-              <p class="text"> Nothing here.</p>
-            </div>
-            <!-- end of summaryEntry -->
-        </div>
-          </div>
-          </section>
+        <section class="pagnation-page" id="4">
+            <?php include 'application/views/quotes/partials/review.php'; ?>
+        </section>
+
           <!--==================================================================-->
           <section class="pagnation-page" id="5">
             <h3 class="hidden">Calculated</h3>
---
+++ application/views/quotes/partials/review.php
@@ -0,0 +1,49 @@
+<div class="row">
+    <div class="columns small-12">
+        <a href="#" id="goBack" data-pag="3" class="sectionTitle">&lt;&nbsp;Input Review</a>
+    </div>
+
+    <div class="columns small-12 medium-6">
+        <div class="summaryEntry" id="sum_details">
+            <h4 class="entryTitle">PROJECT DETAILS<a href="#" class="edit" data-pag="1"><span class="hide-for-mobile">Edit</span><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a></h4>
+            <p class="text"> Nothing here.</p>
+        </div>
+
+        <div class="summaryEntry" id="sum_flow">
+            <h4 class="entryTitle">Flow and velocity<a href="#" class="edit" data-pag="1"><span class="hide-for-mobile">Edit</span><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a></h4>
+            <p class="text"> Nothing here.</p>
+        </div>
+
+        <div class="summaryEntry" id="sum_slopes">
+            <h4 class="entryTitle">Slopes<a href="#" class="edit" data-pag="2"><span class="hide-for-mobile">Edit</span><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a></h4>
+            <p class="text"> Nothing here.</p>
+        </div>
+
+        <div class="summaryEntry" id="sum_type">
+            <h4 class="entryTitle">Types of Flow<a href="#" class="edit" data-pag="2"><span class="hide-for-mobile">Edit</span><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a></h4>
+            <p class="text"> Nothing here.</p>
+        </div>
+    </div>
+
+    <div class="columns small-12 medium-6">
+        <div class="summaryEntry" id="sum_bed">
+            <h4 class="entryTitle">Bed width and alignment<a href="#" class="edit" data-pag="2"><span class="hide-for-mobile">Edit</span><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a></h4>
+            <p class="text"> Nothing here.</p>
+        </div>
+
+        <div class="summaryEntry" id="sum_channel">
+            <h4 class="entryTitle">Channel Specifications<a href="#" class="edit" data-pag="3"><span class="hide-for-mobile">Edit</span><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a></h4>
+            <p class="text"> Nothing here.</p>
+        </div>
+
+        <div class="summaryEntry" id="sum_environment">
+            <h4 class="entryTitle">Environment<a href="#" class="edit" data-pag="3"><span class="hide-for-mobile">Edit</span><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a></h4>
+            <p class="text"> Nothing here.</p>
+        </div>
+
+        <div class="summaryEntry" id="sum_comments">
+            <h4 class="entryTitle">Comments<a href="#" class="edit" data-pag="3"><span class="hide-for-mobile">Edit</span><img src="<?php echo base_url('img/pencil_icon.svg');?>"/></a></h4>
+            <p class="text"> Nothing here.</p>
+        </div>
+    </div>
+</div>
