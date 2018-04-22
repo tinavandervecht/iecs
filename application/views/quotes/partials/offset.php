0e1d1f50273d6e7f981b36f23161f0c91cb5435e
Tina Vandervecht <tina.vandervecht@roomroster.com>
2018-04-22 16:21:51 -0400

    new offset partial

---
+++ application/views/quotes/partials/offset.php
@@ -0,0 +1,32 @@
+<div class="row" id="flow_velocity_inputs">
+    <div class="columns small-12">
+        <h3 class="sectionTitle">Offset Forces</h3>
+    </div>
+
+    <div id="offset"  class="multi_input clearfix columns small-12 medium-6">
+        <h4 class="title">Offset</h4>
+        <label for="offsetMeters">
+            <h5 class="unit">Cubic m/s</h5>
+        </label>
+        <input type="number" id="offsetMeters" class="convert metric required" name="offsetMeters"
+            value="<?php if (isset($_POST['offsetMeters'])) :
+                echo $_POST['offsetMeters'];
+            elseif (isset($estimate)) :
+                echo $estimate['estimate_offset'];
+            else:
+                echo '12.7';
+            endif; ?>"
+        />
+        <?php echo form_error('offsetMeters', '<p class="error">', '</p>');?>
+
+        <label for="offsetFeet">
+            <h5 class="unit">Cubic ft/s</h5>
+        </label>
+        <input type="number" id="offsetFeet" name="offsetFeet" class="convert imperial required"
+            value="<?php if(isset($_POST['offsetFeet'])){echo $_POST['offsetFeet'] ;} ?>"
+        />
+        <?php echo form_error('offsetMeters', '<p class="error">', '</p>');?>
+    </div>
+    <div class="clearfix columns small-12 medium-6">
+    </div>
+</div>
