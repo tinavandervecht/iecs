15d71c1576e5e5d632bb0b1d872d6812633c02b1
Tina Vandervecht <tina.vandervecht@roomroster.com>
2018-05-10 15:40:37 -0400

    I am dumb

--- application/views/templates/quoteNav.php
+++ application/views/templates/quoteNav.php
@@ -17,7 +17,7 @@
                   <img src="<?php echo base_url('img/default_dude_img.png');?>" class="profilepic"/>
                   <h3 class="name"><?php echo $userInfo['company_contactName'];?></h3>
                   <h4 class="title"><?php echo $userInfo['company_name'];?></h4>
-                  <a href="/dashboard/logout"><small>Logout</small></a>
+                  <a href="<?php echo base_url('dashboard/logout'); ?>"><small>Logout</small></a>
                 </section>
                 <nav id="mainNav">
                     <h3 id="navTitle">Navigation</h3>
