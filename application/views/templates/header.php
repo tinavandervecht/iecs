b10ca3b332ab864597e5d858e8335dbcd9b2aaac
Tina Vandervecht <tvandervecht@gmail.com>
2018-11-30 12:29:37 -0500

    Favicon

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,6 +7,15 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title;?></title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url('img/apple-touch-icon.png');?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url('img/favicon-32x32.png');?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url('img/favicon-16x16.png');?>">
+    <link rel="manifest" href="<?php echo base_url('img/site.webmanifest');?>">
+    <link rel="mask-icon" href="<?php echo base_url('img/safari-pinned-tab.svg');?>" color="#4b8709">
+    <meta name="msapplication-TileColor" content="#4b8709">
+    <meta name="theme-color" content="#4b8709">
+
     <link rel="stylesheet" href="<?php echo base_url('fonts/fonts.css');?>">
     <link rel="stylesheet" href="<?php echo base_url('css/foundation.css');?>">
     <link rel="stylesheet" href="<?php echo base_url('css/toastr.css');?>">
